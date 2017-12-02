fa39af76f272affe6ec38802c573117b35bd58b5
Unknown <gyrometor@gmail.com>
2017-12-02 13:48:31 -0500

    Added Project Files

---
+++ Assets/scripts/Shooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public float shootSpeed = 3;
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Camera cam = (Camera)(this.gameObject.GetComponent(typeof(Camera)));
+            
+            GameObject bullInst = Instantiate(bullet, cam.transform.position, cam.transform.rotation);
+            
+            Rigidbody brb = (Rigidbody)(bullInst.GetComponent(typeof(Rigidbody)));
+            
+            brb.velocity = cam.transform.rotation * (new Vector3(0, 0, shootSpeed));
+        }
+    }
+}
