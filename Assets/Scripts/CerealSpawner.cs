abe58721129cc6b16500583945d22645fa4d3ace
sabrinasukhin <sabrina@sukhin.com>
2017-12-02 21:14:36 -0500

    Cereal Spawn Script

---
+++ Assets/Scripts/CerealSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CerealSpawner : MonoBehaviour
+{
+
+    public List<GameObject> spawnList = new List<GameObject>();
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
