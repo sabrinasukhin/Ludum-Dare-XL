e0c3edd5618516a7d22b143eafa1782bb2c2d7d3
sabrinasukhin <sabrina@sukhin.com>
2017-12-02 23:14:31 -0500

    HEALTH BAR

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HungerBar : MonoBehaviour
+public class Health : MonoBehaviour
 {
-    public Slider healthSlider;
+    public GameObject healthBar;
     public float startingHealth = 100.00f;
     public float currentHealth;
     public float decreaseTime = 5.00f;
     bool damaged;
-    
-    void Start ()
+    public Text test;
+
+    void Start()
     {
         currentHealth = startingHealth;
-	}
-	
-	void Update ()
+    }
+
+    void Update()
     {
         currentHealth -= decreaseTime * Time.deltaTime;
+        healthBar.transform.localScale += new Vector3(-0.0001F, 0, 0);
+        test.text = "" + currentHealth;
     }
 }
