ef941373668c8d4a93c6ae334d51029328b4efaa
Unknown <gyrometor@gmail.com>
2017-12-03 11:31:01 -0500

    Add dive moths.

--- Assets/scripts/DiveMothSpawner.cs
+++ Assets/scripts/DiveMothSpawner.cs
@@ -12,16 +12,19 @@ public class DiveMothSpawner : MonoBehaviour
     
     private float timeToNextSpawn;
     private float intervalRange;
+    private Vector3 bottomOrigin;
 
-	// Use this for initialization
 	void Start ()
     {
-		intervalRange = maxInterval - minInterval;
+        Destroy(GetComponent<MeshRenderer>());  //spawner should be invisible
+        
+		intervalRange = maxInterval - minInterval;  //set up timing
         timeToNextSpawn = intervalRange * Random.value;
+        
+        bottomOrigin = (transform.rotation * (new Vector3(0.0f, -transform.localScale.y/2.0f, 0.0f))) + transform.position; //origin of bottom of spawner
 	}
 	
-	// Update is called once per frame
-	void Update ()
+	void Update ()  //handles spawn timing
     {
 		timeToNextSpawn -= Time.deltaTime;
         
@@ -34,7 +37,11 @@ public class DiveMothSpawner : MonoBehaviour
     
     void SpawnMoth()
     {
-        GameObject newMoth = Instantiate(mothPrefab, transform.position, transform.rotation);
+        GameObject newMoth = Instantiate(mothPrefab);
+        
+        newMoth.transform.position = (transform.rotation * (new Vector3((Random.value-0.5f)*transform.localScale.x, newMoth.transform.position.y, (Random.value-0.5f)*transform.localScale.z))) + bottomOrigin;
+        newMoth.transform.rotation = transform.rotation;
+        
         newMoth.GetComponent<MothDiver>().player = player;
     }
 }
