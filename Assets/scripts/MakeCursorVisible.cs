5c76a44f1a33c9caaea49b9e91393eaca9fa4525
Unknown <gyrometor@gmail.com>
2017-12-05 23:30:00 -0500

    Clean up cursor visibility script.

--- Assets/scripts/MakeCursorVisible.cs
+++ Assets/scripts/MakeCursorVisible.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MakeCursorVisible : MonoBehaviour {
-
-	
+public class MakeCursorVisible : MonoBehaviour
+{
 	void Start ()
     {
 		Cursor.lockState = CursorLockMode.None;
