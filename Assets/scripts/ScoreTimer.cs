b9cd34a9b7d990efa0b68f355fc887c2924ef366
Unknown <gyrometor@gmail.com>
2017-12-06 23:15:15 -0500

    Add timer.

---
+++ Assets/scripts/ScoreTimer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTimer : MonoBehaviour
+{
+    static float timeSurvived = 0.0f;
+    float timeStarted;
+
+	void Start ()
+    {
+		if(gameObject.tag == "Player")
+        {
+            timeSurvived = 0.0f;
+            timeStarted = Time.time;
+        }
+        else if(gameObject.tag == "ScoreText")
+        {
+            float oldRecord = PlayerPrefs.GetFloat("highscore", 0.0f); 
+        
+            if(timeSurvived > oldRecord)
+            {
+                PlayerPrefs.SetFloat("highscore", timeSurvived);
+                gameObject.GetComponent<Text>().text = "You survived for " + timeSurvived.ToString() + " seconds.\nThat's a new record!";
+            }
+            else
+                gameObject.GetComponent<Text>().text = "You survived for " + timeSurvived.ToString() + " seconds.\nYour record is " + oldRecord.ToString() + " seconds.";
+        }
+	}
+    
+    void OnDestroy ()
+    {
+        if(gameObject.tag == "Player")
+        {
+            timeSurvived = Time.time - timeStarted;
+        }
+    }
+}
